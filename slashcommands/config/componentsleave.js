29c2cbc1f731ecbde879730d9114e9af90cd02c8
MateoDeveloper <m.medinatech@gmail.com>
2022-03-10 22:56:41 -0500

    add modals to componentsleave command

--- slashcommands/config/componentsleave.js
+++ slashcommands/config/componentsleave.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const { Modal, TextInputComponent, showModal } = require('discord-modals')
 const schema = require("../../models/components-leave")
 /**
 * @type {import('../../types/typeslash').Command}
@@ -17,22 +18,7 @@ const command = {
     .setDescription("Establece el embed personalizado para el sistema de despedidas.")
     .addSubcommand(o => 
         o.setName("set")
-        .setDescription("Ingresa los datos.")
-            .addStringOption(o =>
-              o.setName("descripcion")
-              .setDescription("Descripción que llevará el embed.")
-              .setRequired(true)
-            )
-            .addStringOption(o =>
-              o.setName("titulo")
-              .setDescription("Titulo que llevará el embed.")
-              .setRequired(false)
-            )
-            .addStringOption(o =>
-              o.setName("footer")
-              .setDescription("Footer que llevará el embed")
-              .setRequired(false)
-            )
+        .setDescription("Ingresa los datos.")            
     )
     .addSubcommand(o =>
       o.setName("help")
@@ -46,78 +32,115 @@ const command = {
      * @param {CommandInteraction} interaction 
      */
 
-    async run(client, interaction){
+    async run(client, interaction){      
 
       const args = interaction.options
-      const title = args.getString("titulo")
-      const description = args.getString("descripcion")
-      const footer = args.getString("footer")
-      const subcmd = args.getSubcommand()
-
-      if(!title){
-        title = ""
-      }
-      if(!footer){
-        footer = ""
-      }
+      const subcmd = args.getSubcommand()      
 
       const components = await schema.findOne({ServerID: interaction.guild.id})
 
       if(subcmd === "set"){
 
-      if(!components){
+        const modal = new Modal()
+      .setTitle('Components Leave')
+      .setCustomId('components_leave')
+      .addComponents(
+        new TextInputComponent()
+        .setCustomId('leave_title')
+        .setLabel('Título')
+        .setMinLength(1)
+        .setMaxLength(256)
+        .setStyle('SHORT')
+        .setPlaceholder('Aquí va el título.')
+        .setRequired(false),
+
+        new TextInputComponent()
+        .setCustomId('leave_description')
+        .setLabel('Descripción')
+        .setMinLength(1)
+        .setMaxLength(4000)
+        .setStyle('LONG')
+        .setPlaceholder('Aquí va la descripción.')
+        .setRequired(false),
+
+        new TextInputComponent()
+        .setCustomId('leave_footer')
+        .setLabel('Footer')
+        .setMinLength(1)
+        .setMaxLength(2048)
+        .setStyle('LONG')
+        .setPlaceholder('Aquí va el footer.')
+        .setRequired(false),
+      )
+
+      showModal(modal, { client: client, interaction: interaction })
+
+      client.on('modalSubmit', async(modal) => {
+        const title = modal.getTextInputValue('leave_title')
+        const description = modal.getTextInputValue('leave_description')
+        const footer = modal.getTextInputValue('leave_footer')
+
+        if(!title){
+          title = ""
+        }
+        if(!footer){
+          footer = ""
+        }
+
+        if(!components){
         
-        let compo = new schema({
-          ServerID: interaction.guild.id,
-          Description: description,
-          Title: title,
-          Footer: footer
-        })
-        compo.save()
-
-        let embed = new MessageEmbed()
-        .setTitle("Muy bien! :heart:")
-        .setDescription("He analizado los datos y he guardado la información en mi base de datos.")
-        .setColor("AQUA")
-        .setFooter({text: "Dame unos segundos, editaré este mensaje"})
-
-        const final = new MessageEmbed()
-        .setTitle(title)
-        .setDescription(description)
-        .setFooter({text: footer})
-
-        interaction.reply({embeds: [embed]})
-        setTimeout(() => {
-          interaction.editReply({embeds: [final]})
-        }, 5000)
-
-      } else {
-
-        await schema.updateOne({
-          ServerID: interaction.guild.id,
-          Description: description,
-          Title: title,
-          Footer: footer
-        })
-
-        let embed2 = new MessageEmbed()
-        .setTitle("Muy bien! :heart:")
-        .setDescription("He analizado los datos y he actualizado la información en mi base de datos.")
-        .setColor("AQUA")
-        .setFooter({text: "Dame unos segundos, editaré este mensaje"})
-
-        const final = new MessageEmbed()
-        .setColor("FUCHSIA")
-        .setTitle(title)
-        .setDescription(description)
-        .setFooter({text: footer})
-
-        interaction.reply({embeds: [embed2]})
-        setTimeout(() => {
-          interaction.editReply({embeds: [final]})
-        }, 5000)
-
-      }
+          let compo = new schema({
+            ServerID: interaction.guild.id,
+            Description: description,
+            Title: title,
+            Footer: footer
+          })
+          compo.save()
+  
+          let embed = new MessageEmbed()
+          .setTitle("Muy bien! :heart:")
+          .setDescription("He analizado los datos y he guardado la información en mi base de datos.")
+          .setColor("AQUA")
+          .setFooter({text: "Dame unos segundos, editaré este mensaje"})
+  
+          const final = new MessageEmbed()
+          .setTitle(title)
+          .setDescription(description)
+          .setFooter({text: footer})
+  
+          interaction.reply({embeds: [embed]})
+          setTimeout(() => {
+            interaction.editReply({embeds: [final]})
+          }, 5000)
+  
+        } else {
+  
+          await schema.updateOne({
+            ServerID: interaction.guild.id,
+            Description: description,
+            Title: title,
+            Footer: footer
+          })
+  
+          let embed2 = new MessageEmbed()
+          .setTitle("Muy bien! :heart:")
+          .setDescription("He analizado los datos y he actualizado la información en mi base de datos.")
+          .setColor("AQUA")
+          .setFooter({text: "Dame unos segundos, editaré este mensaje"})
+  
+          const final = new MessageEmbed()
+          .setColor("FUCHSIA")
+          .setTitle(title)
+          .setDescription(description)
+          .setFooter({text: footer})
+  
+          interaction.reply({embeds: [embed2]})
+          setTimeout(() => {
+            interaction.editReply({embeds: [final]})
+          }, 5000)
+  
+        }
+      })      
 
     }
 
