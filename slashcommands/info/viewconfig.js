d8bc632aa795ad5c923d4f729db4bde554d4121e
Paulo Rivas <domingomartinez1621@gmail.com>
2022-04-09 16:13:14 -0400

    edit viewconfig

--- slashcommands/info/viewconfig.js
+++ slashcommands/info/viewconfig.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 const welcomeModel = require('../../models/setWelcome');
+const leaveModel = require("../../models/setLeave")
+const componentslLeave = require("../../models/components-leave")
 const componentsModel = require("../../models/components-welcome")
 const inviteModel = require("../../models/setinvitechannel")
 const antilinksModel = require('../../models/antilinks');
@@ -19,6 +21,8 @@ module.exports = {
 
         // Models
         const welcomeChannel = await welcomeModel.findOne({ ServerID: interaction.guild.id })
+        const leaveChannel = await leaveModel.findOne({ServerID: interaction.guild.id })
+        const leaveComponents = await componentslLeave.findOne({ServerID: interaction.guild.id })
         const invitesChannel = await inviteModel.findOne({ServerID: interaction.guild.id})
         const components = await componentsModel.findOne({ServerID: interaction.guild.id})
         const Antilinks = await antilinksModel.findOne({ ServerID: interaction.guild.id })
@@ -39,8 +43,10 @@ module.exports = {
         .setColor('LIGHT_GREY')
         .setDescription('Tu configuración actual:')
         .addField(`📚 Canales`,'*Configuración de canales:*')
-        .addField('🔔 Bienvenidas / Despedidas ',`${welcomeChannel ? `${checked} **Activado**` : `${unchecked} **Desactivado**`}`,true)
+        .addField('🔔 Bienvenidas',`${welcomeChannel ? `${checked} **Activado**\n<#${welcomeChannel.ChannelID}>` : `${unchecked} **Desactivado**`}`,true)
+        .addField('😟 Despedidas',`${leaveChannel ? `${checked} **Activado**\n<#${leaveChannel.ChannelID}>` : `${unchecked} **Desactivado**`}`,true)
         .addField('🔔 Bienvenidas Personalizada ',`${components ? `${checked} **Activado**` : `${unchecked} **Desactivado**`}`,true)
+        .addField('😟 Despedidas Personalizada ',`${leaveComponents ? `${checked} **Activado**` : `${unchecked} **Desactivado**`}`,true)
         .addField('✨ Invitaciones ',`${invitesChannel ? `${checked} **Activado**` : `${unchecked} **Desactivado**`}`,true)
         .addField('👮‍♂️ Auto-Moderación',`*Tu configuración de moderación*`)
         .addField('🔍 Anti-Links',`${Antilinks ? `${checked} **Activado**`: `${unchecked} **Desactivado**`}`,true)
