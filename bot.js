fdc68f1c683f2a8e5089744c36850a69125b49c3
Paulo Rivas <domingomartinez1621@gmail.com>
2022-05-18 19:06:06 -0400

    edit bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,11 @@ const { ShardingManager } = require('discord.js');
 const fs = require("fs");
 const toml = require("toml");
 const config = toml.parse(fs.readFileSync("./config/config.toml", "utf-8"));
+const { cyan } = require("colors")
 
 const manager = new ShardingManager('./index.js', { token:  config.token });
 
 manager.on('shardCreate', (shard) => console.log(`Lanzando shard ${shard.id}`));
+manager.on('shardError', (err) => console.log(cyan(`Error \n ${err}`)))
 
 manager.spawn();
