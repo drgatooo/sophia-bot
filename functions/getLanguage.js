1865326c1041c47c64c375ec2e76e52601a4f2cc
RistDev <contact.ristodev@gmail.com>
2022-08-17 19:31:12 -0500

    getLanguage ahora soporta strings y guilds, además fue mejorado cuando una key no la encuentre

--- functions/getLanguage.js
+++ functions/getLanguage.js
@@ -3,15 +3,22 @@ const { CommandInteraction, Client } = require('discord.js-light')
 
 /**
  * @param {Client} client
- * @param {CommandInteraction} interaction
+ * @param {CommandInteraction | String | Guild} interaction
  * @param {String} keys
  * @return {String[]}
  */
 
 module.exports = (client, interaction, ...keys) => {
-	return keys.map((key) =>
-		client.language.get(key)[interaction.locale]
-			? client.language.get(key)[interaction.locale].replace('&44', ',')
-			: client.language.get(key)['en-US'].replace('&44', ','),
-	)
+	const lang = interaction.locale || interaction.preferredLocale || interaction
+	return keys.map((key) => {
+		const k = client.language.get(key)
+		if (k?.[lang]) {
+			return k[lang]
+		} else if (k?.['es-ES']) {
+			return k['es-ES']
+		} else {
+			console.log('No se encontró la key', key)
+			return key
+		}
+	})
 }
